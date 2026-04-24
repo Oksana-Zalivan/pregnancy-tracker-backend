e117c5d29cee72c7b2af77ddaedd15ad9fa6edc7
Bohdan_Dluzhanskyi <kvant0109@gmail.com>
2026-04-24 18:27:46 +0300

    feat: add endpoint for user avatar update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,8 @@
+import createHttpError from 'http-errors';
+
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { User } from '../models/User.js';
+
 export const getCurrentUserController = (req, res) => {
   res.json({
     message: "Поточний користувач успішно отриманий",
@@ -8,3 +13,25 @@ export const getCurrentUserController = (req, res) => {
     },
   });
 };
+
+export async function updateUserAvatar(req, res) {
+  if (!req.file) {
+    throw createHttpError(400, 'No file');
+  }
+
+  const result = await saveFileToCloudinary(req.file.buffer);
+  if (!result) {
+    throw createHttpError(500, 'Error of upload avatar(');
+  }
+
+  const user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { avatar: result.secure_url },
+    { returnDocument: 'after' },
+  );
+  if (!user) {
+    throw createHttpError(500, 'Sorry, error of update user-avatar');
+  }
+
+  res.status(200).json({ url: user.avatar });
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
 import { authenticate } from "../middlewares/authenticate.js";
-import { getCurrentUserController } from "../controllers/userController.js";
+import { getCurrentUserController,updateUserAvatar } from "../controllers/userController.js";
+import { upload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
 router.get("/current", authenticate, getCurrentUserController);
 
+router.patch(
+  '/current/avatar',
+  authenticate,
+  upload.single('avatar'),
+  updateUserAvatar,
+);
 export default router;
