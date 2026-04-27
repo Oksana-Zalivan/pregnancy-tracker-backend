03bbe1be2f25f3703c0c5b38d508f39571813bf5
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-04-27 09:22:24 +0300

    add loginUser logic wip

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,5 @@
 import { User } from "../models/User.js";
+import crypto from "crypto";
 
 export const registerUser = async (payload) => {
   const existingUser = await User.findOne({ email: payload.email });
@@ -10,4 +11,22 @@ export const registerUser = async (payload) => {
   const user = await User.create(payload);
 
   return user;
+};
+
+// This is the User Login logic
+export const loginUser = async (payload) => {
+  const user = await User.findOne({ email: payload.email });
+
+  if (!user) {
+    throw new Error("Користувач з таким email чи паролем не існує!");
+  }
+
+  return user;
+};
+
+// This is the User password logic
+export const passwordUser = async (payload) => {
+  const userPassword = await User.findOne({ password: payload.password });
+
+  return userPassword;
 };
