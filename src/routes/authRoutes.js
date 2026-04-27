e07a9257aa60c90ea9470f0521ee9f319f4aa0aa
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-04-27 18:56:49 +0300

    add login, logout routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,16 @@
-import express from "express";
-import { registerController } from "../controllers/authController.js";
-import { validateBody } from "../middlewares/validateBody.js";
-import { registerUserSchema } from "../validation/auth.js";
+import express from 'express';
+import {
+  registerController,
+  loginUserController,
+  logoutUserController,
+} from '../controllers/authController.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { registerUserSchema, loginUserSchema } from '../validation/auth.js';
 
 const router = express.Router();
 
-router.post("/register", validateBody(registerUserSchema), registerController);
+router.post('/register', validateBody(registerUserSchema), registerController);
+router.post('/login', validateBody(loginUserSchema), loginUserController);
+router.post('/logout', logoutUserController);
 
 export default router;
