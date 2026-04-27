d8330cd55c585f3c22b85170c39245097e600d14
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-04-27 09:22:04 +0300

    add login logic wip

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,6 @@
 import { registerUser } from "../services/authService.js";
+import { loginUser } from "../services/authService.js";
+import bcrypt from "bcrypt";
 
 export const registerController = async (req, res, next) => {
   try {
@@ -12,6 +14,22 @@ export const registerController = async (req, res, next) => {
         email: user.email,
       },
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Login User
+
+export const loginController = async (req, res) => {
+  try {
+    const user = await loginUser(req.body);
+
+    const password = await passwordUser(req.body);
+
+    const isValidPassword = await bcrypt;
+
+    res.status(200).json({ user });
   } catch (error) {
     next(error);
   }
