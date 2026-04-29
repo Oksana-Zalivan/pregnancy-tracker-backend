1c5cc3f9e6e1823809b5a5acdc3a4507bf8e3afd
Oksana Zalivan <ksyushamartinovska@gmail.com>
2026-04-29 20:08:16 +0300

    fix routes

--- src/routes/diaryRoutes.js
+++ src/routes/diaryRoutes.js
@@ -1,18 +1,18 @@
-import express from "express";
-import { celebrate } from "celebrate";
-import { authenticate } from "../middlewares/authenticate.js";
+import express from 'express';
+import { celebrate } from 'celebrate';
+import { authenticate } from '../middlewares/authenticate.js';
 import {
   getDiaries,
   createDiary,
   updateDiary,
   deleteDiary,
 } from '../controllers/diaryController.js';
-import { createDiarySchema } from "../validation/diary.js";
+import { createDiarySchema } from '../validation/diary.js';
 
 const router = express.Router();
 
-router.get("/", authenticate, getDiaries);
-router.post("/", authenticate, celebrate(createDiarySchema), createDiary);
+router.get('/', authenticate, getDiaries);
+router.post('/', authenticate, celebrate(createDiarySchema), createDiary);
 router.patch('/:id', authenticate, updateDiary);
 router.delete('/:id', authenticate, deleteDiary);
 
